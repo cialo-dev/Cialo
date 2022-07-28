87dddb676486ea589d4b0046952f1f6028fca9a1
Mieszko <mieszkomisztal@outlook.com>
2022-07-28 13:44:32 +0200

    Chart

--- app/src/main/java/com/example/cialo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cialo/ui/home/HomeFragment.kt
@@ -1,39 +1,56 @@
 package com.example.cialo.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.cialo.databinding.FragmentHomeBinding
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var _chart: PieChart;
+    private lateinit var _viewModel: HomeViewModel;
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        val viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+        _viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _chart = binding.chart;
 
-        initializeBindings(viewModel)
+        initializeBindings()
+
 
         return binding.root
     }
 
-    private fun initializeBindings(viewModel: HomeViewModel) {
-        val button: Button = binding.buttonCleanDb;
+    private fun initializeBindings() {
+        val entries = mutableListOf<PieEntry>()
 
-        button.setOnClickListener(View.OnClickListener {
-            viewModel.cleanDatabase()
-        })
+        entries.add(PieEntry(55f, "Token"))
+        entries.add(PieEntry(99f, "Nastepny"))
+
+        val dataSet = PieDataSet(entries, "")
+        dataSet.setColors(Color.MAGENTA, Color.GRAY)
+        val l: Legend = _chart.legend
+        l.isEnabled = false;
+        val data = PieData(dataSet)
+
+        _chart.data = data;
+        _chart.invalidate()
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/cialo/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/cialo/ui/home/HomeViewModel.kt
@@ -22,9 +22,7 @@ class HomeViewModel : CialoViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
-    val text: LiveData<String> = _text
 
-    fun cleanDatabase() {
 
-    }
+
 }
